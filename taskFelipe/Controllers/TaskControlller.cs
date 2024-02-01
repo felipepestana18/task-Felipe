82ed30804a66ad820fa8e818c0c231ff44fa97e3
felipepestana <felipe-pe27@hotmail.com>
2024-02-01 10:06:23 -0300

    add: migration

---
+++ taskFelipe/Controllers/TaskControlller.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using taskFelipe.Data.ValueObject;
+
+namespace taskFelipe.Controllers
+{
+    [ApiController]
+    [Route("api/v1/Task/")]
+    public class ClienteController : Controller
+    {
+        readonly ITaskRepository _taskRepository;
+
+        public ClienteController(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [HttpGet("find-all")]
+        public async Task<ActionResult<TaskVO>> GetAll()
+        {
+            var clientes = await _taskRepository.GetAll();
+            if (clientes == null) return NotFound();
+            return Ok(clientes);
+        }
+
+        [HttpGet("find-by-id/{id}")]
+        public async Task<ActionResult<TaskVO>> GetById(int id)
+        {
+            var cliente = await _taskRepository.GetById(id);
+            if (cliente == null) return NotFound();
+            return Ok(cliente);
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<TaskVO>> Create(TaskVO vo)
+        {
+            var cliente = await _taskRepository.Create(vo);
+            if (cliente == null) return NotFound();
+            return Ok(cliente);
+        }
+
+        [HttpPut("update")]
+        public async Task<ActionResult<TaskVO>> Update(TaskVO vo)
+        {
+            var cliente = await _taskRepository.Update(vo);
+            if (cliente == null) return NotFound();
+            return Ok(cliente);
+        }
+
+        [HttpDelete("delete{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            var status = await _taskRepository.Delete(id);
+            if (!status) BadRequest();
+            return Ok(status);
+        }
+
+
+        [HttpDelete("delete{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            var status = await _taskRepository.Delete(id);
+            if (!status) BadRequest();
+            return Ok(status);
+        }
+
+    }
+}
---
+++ taskFelipe/Migrations/20240201125809_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace taskFelipe.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    completed_at = table.Column<bool>(type: "tinyint(1)", nullable: true),
+                    created_at = table.Column<bool>(type: "tinyint(1)", nullable: true),
+                    updated_at = table.Column<bool>(type: "tinyint(1)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
