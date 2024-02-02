92eb98496730fca81a2579cb45a8df5b7063090e
felipepestana <felipe-pe27@hotmail.com>
2024-02-02 14:24:57 -0300

    add; estruture project angular

--- taskFelipe-front/src/app/app.module.ts
+++ taskFelipe-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FormTaskComponent } from './pages/form-task/form-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DashboardComponent,
+    FormTaskComponent
   ],
   imports: [
     BrowserModule,
