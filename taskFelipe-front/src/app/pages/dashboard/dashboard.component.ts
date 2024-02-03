5a901034e316944c1382049e4dbc1349475ed614
felipepestana <felipe-pe27@hotmail.com>
2024-02-03 17:00:30 -0300

    add: dashboard task

--- taskFelipe-front/src/app/pages/dashboard/dashboard.component.ts
+++ taskFelipe-front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  tasks: any;
+  filterTask: String = '';
+  ngOnInit(): void {
+    this.getDataTask();
+  }
+  constructor(private taskService: TaskService, private router: Router) { }
+
+  getDataTask() {
+    this.taskService.getData().subscribe((data: any) => {
+      console.log(data);
+      this.tasks = data;
+    })
+  }
+
+  deleteTask(id: number) {
+
+    this.taskService.deleteData(id).subscribe((data: any) => {
+      this.getDataTask();
+    })
+  }
+
+  updateTask(id: number) {
+    this.router.navigateByUrl('create/' + id);
+  }
+
+  taskDone(id: number) {
+    this.taskService.doneData(id).subscribe((data: any) => {
+      this.getDataTask();
+    })
+  }
+  reserarchTask() {
+    if (this.filterTask) {
+      this.taskService.getDataFilter({ title: this.filterTask }).subscribe((data: any) => {
+        this.tasks = data;
+      })
+    }
+  }
 
 }
