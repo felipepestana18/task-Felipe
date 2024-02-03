7d73dcf4b8333d44ebdf35b0742213bc0a6bd8b2
felipepestana <felipe-pe27@hotmail.com>
2024-02-03 16:59:42 -0300

    add: services request

---
+++ taskFelipe-front/src/app/services/task.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TaskService {
+
+  private url: string = "http://localhost:4400/api/v1/task/";
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  getData() {
+    return this.http.get(this.url + 'find-all');
+  }
+
+  getDataFilter(filter: any) {
+    console.log(filter);
+    return this.http.post(this.url + 'task-filter', filter)
+  }
+
+  postData(formData: any) {
+    return this.http.post(this.url + 'create', formData);
+  }
+
+  putData(id: number, formData: any) {
+    return this.http.put(this.url + 'update', formData);
+  }
+
+  deleteData(id: number) {
+    return this.http.delete(this.url + 'delete/' + id);
+  }
+
+  doneData(id: number) {
+    return this.http.put(this.url + 'done/' + id, '');
+  }
+}
